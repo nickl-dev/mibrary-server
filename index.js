e104ab35834988de017c0191036f96b46160b129
NicholasSlade <60986212+NicholasSlade@users.noreply.github.com>
2021-12-15 22:00:39 -0800

    -added semicolons

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-const dotenv = require('dotenv')
-dotenv.config()
-const port = process.env.SERVER_PORT || 4000
-const express = require('express')
+const dotenv = require('dotenv');
+dotenv.config();
+const port = process.env.SERVER_PORT || 4000;
+const express = require('express');
 const app = express();
-const cors = require('cors')
-const bodyParser = require('body-parser')
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-app.use(cors)
-app.use(bodyParser.json())
+app.use(cors);
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send({
-    year: new Date(Date.now()).getFullYear(),
-    nextYear: this.year + 1
-  })
-})
+app.get('/', (req, res) => res.send('Hello world'));
 
-app.listen(port, () => console.log(`Server is listening at ${port} @ ${new Date(Date.now())}`))
+app.listen(port, () => console.log(`Server is listening at ${port} @ ${new Date(Date.now())}`));
