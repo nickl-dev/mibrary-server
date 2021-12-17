2dde32d6696cd4771adadcb16fce195f8b37d3a2
NicholasSlade <60986212+NicholasSlade@users.noreply.github.com>
2021-12-16 23:06:41 -0800

    -invoked cors, fixed server not responding issue

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config()
 const port = process.env.SERVER_PORT || 4000;
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-app.use(cors);
+app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello world'));
+app.get('/', (req, res) => {
+  try {
+    res.send('Hello world')
+  } catch (err) {
+    console.error(err)
+  }
+})
+
+app.post('/', (req, res) => {
+  try {
+    console.log(req)
+  } catch (err) {
+    console.errer(err)
+  }
+})
 
 app.listen(port, () => console.log(`Server is listening at ${port} @ ${new Date(Date.now())}`));
