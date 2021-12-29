d27528f31c96493de52247bb40ff9de99b0113e3
NicholasSlade <60986212+NicholasSlade@users.noreply.github.com>
2021-12-28 16:23:14 -0800

    -changed route to /

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const connection = mysql.createConnection({
 // Connection to database
 connection.connect()
 
-app.get('/register', (req, res) => {
+app.get('/', (req, res) => {
     connection.query('SELECT * FROM USERS', (err, rows, fields) => {
       if (err) {
         throw err
@@ -42,7 +42,7 @@ app.get('/register', (req, res) => {
     })
 })
 
-app.post('/register', (req, res) => {
+app.post('/', (req, res) => {
   console.log(req.body)
   res.status(200).send('Posted')
 })
