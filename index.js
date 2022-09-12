88ea3399ebae5778ef34b2fa53e49d60132ca500
Nicholas Lal <nlalkta@gmail.com>
2022-09-12 13:28:32 -0600

    -changed author in package.json -refactored starter mySQL code

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-require('dotenv').config()
-const port = process.env.SERVER_PORT || 4000
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const bodyParser = require('body-parser')
+require('dotenv').config();
+const port = process.env.SERVER_PORT || 4000;
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.listen(port, () => console.log(`Server is listening at ${port} @ ${new Date(Date.now())}`))
+app.listen(port, () => console.log(`Server is listening at ${port} @ ${new Date(Date.now())}`));
 
 // Database
-const mysql = require('mysql')
+const mysql = require('mysql');
 
 // Database credentials
-const DB_HOST = process.env.DB_HOST
-const DB_USER = process.env.DB_USER
-const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME
 
 // Creating connection to database
@@ -26,24 +26,30 @@ const connection = mysql.createConnection({
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME
-})
+});
+
+// Reusable query funciton
+function mySQLQuery (query, message) {
+  connection.query(query, (error, result) => {
+    if (error) throw error;
+
+    console.log({
+      result, 
+      message
+    })
+  })
+}
+
+// Query strings
+const CREATE_TABLE_QUERY = 'CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), email_address VARCHAR(255), password VARCHAR(255))';
+const INSERT_USER_QUERY = 'INSERT INTO users (name, email_address, password) VALUES ("Frederick", "frederick_forsyth@gmail.com", "i-like-thrillers-1938")';
 
 // Connection to database
-connection.connect()
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected');
 
-app.post('/', (req, res) => {
-  const { email, password } = req.body
-    connection.query(`SELECT * FROM users WHERE email = ${email} AND password = ${password}`, (err, rows, fields) => {
-      if (err) {
-        throw err
-      } else {
-        res.send('User exists')
-      }
-      console.log(email, password)
-    })
-})
+  // if (mySQLQuery(CREATE_TABLE_QUERY, 'Table created');
 
-app.post('/', (req, res) => {
-  console.log(req.body)
-  res.status(200).send('Posted')
-})
+  mySQLQuery(INSERT_USER_QUERY, 'User inserted');
+});
